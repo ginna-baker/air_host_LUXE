32a9601d4ffa03d02c0ab4d92ffc3ce711e419f2
ginna-baker <ginna.baker@gmail.com>
2014-09-18 17:11:38 -0400

    before ng model refactoring

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -22,6 +22,11 @@ angular.module('airhostluxeApp')
       if($scope.newListing === '') {
         return;
       }
+
+      var postObj = {};
+
+      postObj.property_info = $scope.listing.property_info;
+
       $http.post('/api/listings/', {
         "date": new Date(),
         "user": {
@@ -29,35 +34,37 @@ angular.module('airhostluxeApp')
           "profile_photo": $scope.listing.profile_photo
         },
         "property_info": {
-          "name": $scope.listing.propoerty_name,
-          "street_address": $scope.listing.street_address,
-          "city": $scope.listing.city,
-          "country": $scope.listing.country,
-          "airbnb_url": $scope.listing.airbnburl,
+          "name": $scope.listing.property_info.propoerty_name,
+          "big_photo": $scope.listing.property_info.photo,
+          "tagline": $scope.listing.property_info.tagline,
+          "street_address": $scope.listing.property_info.street_address,
+          "city": $scope.listing.property_info.city,
+          "country": $scope.listing.property_info.country,
+          "airbnb_url": $scope.listing.property_info.airbnburl,
+          "airbnb_rating": $scope.listing.property_info.airbnbrating,
+          "airbnb_guest_count": $scope.listing.propoerty_info.airbnb_guest_count,
           "luxe_url": $scope.user.name +"/"+ $scope.generateUrlName()
         },
-        "site_color": $scope.listing.site_color,
-        "photo": $scope.listing.photo,
-        "welcome_msg": $scope.listing.welcome_msg,
         "your_stay": {
           "msg": $scope.listing.your_stay.msg,
-          "wireless_pwd": $scope.listing.your_stay_msg.wireless_pwd
+          "house_info": $scope.listing.your_stay.house_info,
+          "tv_instructions": $scope.listing.your_stay.wireless_pwd,
+          "wireless_pwd": $scope.listing.your_stay.wireless_pwd,
+          "rules": $scope.listing.your_stay.house_rules
         },
         "what_to_do": {
           "msg": $scope.listing.what_to_do.msg,
-          "photo": $scope.listing.what_to_do.photo,
+          "place_name": $scope.listing.what_to_do.place_name,
           "place_address": $scope.listing.what_to_do.place_address,
           "place_note": $scope.listing.what_to_do.place_note
         },
         "where_to_eat": {
           "msg": $scope.listing.where_to_eat.msg,
-          "photo": $scope.listing.where_to_eat.photo,
           "place_name": $scope.listing.where_to_eat.place_address,
           "place_address": $scope.listing.where_to_eat.place_address,
           "place_note": $scope.listing.where_to_eat.place_note
         },
         "emergency": {
-          "photo": $scope.listing.emergency.photo,
           "call_number": $scope.listing.emergency.call_number,
           "poison_ctrl_center": $scope.listing.emergency.poison_ctrl_center,
           "first_aid_kit": $scope.listing.emergency.first_aid_kit,
